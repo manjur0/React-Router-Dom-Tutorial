54140008a40c1b14a407a40c29de6c347b98cafb
manjur0 <sfmanjur21@gmail.com>
2024-09-18 08:23:07 +0600

    nested route and Outlet and other functionalily added

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,18 @@ import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Root from "./Root";
 import ErrorPage from "./Error";
+import Contact from "./Contact";
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
     errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/contacts/:contactId",
+        element: <Contact />,
+      },
+    ],
   },
 ]);
 
